a03bb01943ebb309bc11345a7dedb6489c47b1a6
alexandre auriol <auriol.alexandre@gmail.com>
2019-08-27 11:05:05 +0200

    add css form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   has_many :users_todolists, dependent: :destroy
   has_many :todolists, through: :users_todolists
   has_many :listitems, through: :todolists, class_name: 'Listitem', source: :listitems
 
   def get_items
-     self.todolists.map { |todo| todo.listitems }.flatten
-  end
+   self.todolists.map { |todo| todo.listitems }.flatten
+ end
 
 end
